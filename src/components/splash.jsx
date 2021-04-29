2b70f56b629f40f5d8c8518d69da044f8e4a3f12
Edward Kim <eykim5@buffalo.edu>
2021-04-28 22:53:10 -0400

    Added containers, text and images.

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './oticon-logo.png'
 import './splash.css'
 
 class Splash extends React.Component {
@@ -7,10 +8,14 @@ class Splash extends React.Component {
             <div className = "mainBox">
                 <div className = "topBox">
                     <div className = "leftBox">
-                        <div>$500 off Oticon More Technology</div>
+                        <div className = "logoImg">
+                            <img src={logo}></img>
+                        </div>
+                        <div className = "offerHead">$500 off Oticon More Technology</div>
+                        <div className = "offerText">(TEMPORARY) All of our hearing solutions are built on BrainHearing™ technology that is designed to support your brain’s natural process of making sense of sound. Through life-changing technology, we are able to provide solutions for all types of hearing loss, ages and lifestyles. We design our hearing aid solutions to meet your hearing needs and provide reliable, discreet and comfortable options — specifically for you!</div>
+                        <div className = "offerButton">Learn more</div>
                     </div>
                     <div className = "rightBox">
-                        <div>byeah</div>
                     </div>
                 </div>
                 <div className = "botBox">
