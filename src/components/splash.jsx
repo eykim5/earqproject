6449c4693f2c2a74b4fce57c36296d6bbe162a7c
Edward Kim <eykim5@buffalo.edu>
2021-04-28 20:50:47 -0400

    Basic formatting created with sample text.

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -4,7 +4,19 @@ import './splash.css'
 class Splash extends React.Component {
     render() {
         return(
-            <div className = "basics">Hello World</div>
+            <div className = "mainBox">
+                <div className = "topBox">
+                    <div className = "leftBox">
+                        <div>$500 off Oticon More Technology</div>
+                    </div>
+                    <div className = "rightBox">
+                        <div>byeah</div>
+                    </div>
+                </div>
+                <div className = "botBox">
+                    <div>Hello World</div>
+                </div>
+            </div>
         )
     }
 }
