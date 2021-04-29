0e8a5d15beed6c12ff4783f91bc959f75bf66bf0
Edward Kim <eykim5@buffalo.edu>
2021-04-28 20:28:16 -0400

    Added package files, edited App.js and added basic splash files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { Route} from "react-router-dom";
+import Splash from './components/splash';
+import { HashRouter } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <HashRouter>
+      <Route path ='/' component={Splash}/>
+
+    </HashRouter>
   );
+
 }
 
 export default App;
---
+++ src/components/splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './splash.css'
+
+class Splash extends React.Component {
+    render() {
+        return(
+            <div className = "basics">Hello World</div>
+        )
+    }
+}
+
+export default Splash;
