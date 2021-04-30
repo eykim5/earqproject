34b5bef30937a9f75c572d4ae470281fecfe6c32
Edward Kim <eykim5@buffalo.edu>
2021-04-30 00:45:33 -0400

    Updates

---
+++ precache-manifest.c7ebc90b406d7c8c55ea791f4ba62f7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c1b30e73fc264fabb106b17d67317c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c33a792bb8d174eab2c",
+    "url": "/static/css/main.39d05e6d.chunk.css"
+  },
+  {
+    "revision": "ed117a19d18a9dd2b1a8",
+    "url": "/static/js/2.eb6f1cdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb6f1cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1482a4bb2c41c5b259",
+    "url": "/static/js/3.bc35aabb.chunk.js"
+  },
+  {
+    "revision": "2c33a792bb8d174eab2c",
+    "url": "/static/js/main.ea150afa.chunk.js"
+  },
+  {
+    "revision": "dfa2ec883cd43c6f892c",
+    "url": "/static/js/runtime-main.e8b30611.js"
+  },
+  {
+    "revision": "b6323964ba95075ac7e8398f2caa5ad4",
+    "url": "/static/media/oticonmoreblack.b6323964.png"
+  }
+]);
